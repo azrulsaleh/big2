3beb08fe631453d0cbadca363b98c45c85e36579
Azrul Saleh <azrulsaleh@me.com>
2026-06-16 19:23:53 +0800

    feat: added stripe to background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 export default function App() {
-  return (
-    <div></div>
-  );
+	return (
+		<div>
+			<div className='
+				absolute
+				-z-10
+			'>
+				<svg
+					width="100vw"
+					height="100vh"
+					viewBox="0 0 100 100"
+				>
+					<polygon
+						points="100,-100 200,-100 0,200 -100,200"
+						fill="var(--color-a1)"
+						stroke="var(--color-a2)"
+						stroke-width="0.1"
+					/>
+				</svg>
+			</div>
+			<div className='relative' />
+		</div>
+	);
 }
