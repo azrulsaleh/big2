879b0ebb5b06f20bf015cee5695f51b82a951766
Azrul Saleh <azrulsaleh@me.com>
2026-06-16 20:52:28 +0800

    feat: added big 2 title

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,26 @@ export default function App() {
 					/>
 				</svg>
 			</div>
-			<div className='relative' />
+			<div className="
+				fixed
+				justify-center
+				top-2/5 left-1/2 -translate-1/2
+				text-n6
+			">
+				<div className='flex flex-row gap-[clamp(1rem,5vw,10rem)] items-center'>
+					<div className='
+						flex flex-row
+						gap-[clamp(1rem,5vw,10rem)]
+						font-thin
+						text-[clamp(7rem,15vw,15rem)]
+					'>
+						<h1>B</h1>
+						<h1>I</h1>
+						<h1>G</h1>
+					</div>
+					<h1 className='text-[clamp(15rem,25vw,25rem)] font-extrabold'>2</h1>
+				</div>
+			</div>
 		</div>
 	);
 }
